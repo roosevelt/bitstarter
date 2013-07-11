d26d94bc8452abcbe9cad62b00ad705c9ba39a18
roosevelt <roosevelt1@gmail.com>
2013-07-11 15:00:01 +0000

    updated webjs to get content from index

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
+var bf = require('buffer');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    buff= fs.readFileSync("index.html");
+    resp = buff.toString();
+    response.send(resp);
 });
 
 var port = process.env.PORT || 5000;
